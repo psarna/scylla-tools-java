16f75802cc3930c70df1560f42f3a77bc6015a08
Pekka Enberg <penberg@scylladb.com>
2018-08-04 18:42:21 +0000

    BulkLoader: Make send() thread-safe
    We see ConcurrentModificationExceptions in
    BatchStatement.ensureAllSet(), which means that some other thread is
    mutating the batch statement in the background. One possible such case
    is when a deferring future from sendPrepared() completes but another
    thread is in send(Object, DecoratedKey, Statement) and has already
    passed the BatchStatement to executeAsync() but has not set
    batchStatement to null.
    
    Fix the issue by making send() synchronized to make sure only one thread
    (from a deferring future, for example) accesses batchStatement at a
    time.
    
    Fix suggested by Piotr Jastrzebski.

--- src/java/com/scylladb/tools/BulkLoader.java
+++ src/java/com/scylladb/tools/BulkLoader.java
@@ -478,7 +478,7 @@ public class BulkLoader {
         }
 
         @Override
-        public void finish() {
+        public synchronized void finish() {
             if (batchStatement != null && !batchStatement.getStatements().isEmpty()) {
                 send(batchStatement);
                 batchStatement = null;
@@ -517,7 +517,10 @@ public class BulkLoader {
             }
         }
 
-        private void send(Object callback, DecoratedKey key, Statement s) {
+        // This method has to be synchronized because it can be called from different threads.
+        // Usually it's called just from a single thread - the one that owns the client but for
+        // prepared statements it's called from callbacks that have their own thread pool which runs them.
+        private synchronized void send(Object callback, DecoratedKey key, Statement s) {
             if (batch && tokenKey == callback && batchStatement != null && batchStatement.requestSizeInBytes(PROTOCOL_VERSION, codecRegistry) < maxBatchSize
                     && this.key.equals(key)) {
                 batchStatement.add(s);
